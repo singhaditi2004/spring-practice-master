ff98574b72db98a7da9dc5d435d07f2467d9dce7
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-08-30 22:37:23 +0530

    annotations

--- src/main/java/com/springcore/master/master/spring/Doctor.java
+++ src/main/java/com/springcore/master/master/spring/Doctor.java
@@ -1,6 +1,10 @@
 package com.springcore.master.master.spring;
 
-public class Doctor implements Staff {
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.BeanNameAware;
+
+public class Doctor implements Staff,BeanNameAware {
 
 	private String qual;
 	private Nurse nurse;
@@ -28,5 +32,17 @@ public class Doctor implements Staff {
 	@Override
 	public String toString() {
 		return "Doctor [qual=" + qual + ", nurse=" + nurse + "]";
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("Set bean name is called");
+		
+	}
+	
+	@PostConstruct
+	public void postConst() {
+		System.out.println("Post constructor is called");
 	}
 }
